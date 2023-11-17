5960dbbae9a45a83b1b8961835c2978c4739b1a6
Vijayant <vijayantsingh123@gmail.com>
2023-11-17 21:49:07 +0530

    comfystays(real state booking website)

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {Auth0Provider} from "@auth0/auth0-react"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider domain="dev-4fx4okyyyrx1znq8.us.auth0.com" clientId="AzheCqiLQbnXXaa4kDaTLQy8f7mo4kfO"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+    }}
+    audience="http://localhost:8000"
+    scope="openid profile email"
+    >
+    <App />
+    </Auth0Provider>
+   
+  </React.StrictMode>
+);
---
+++ frontend/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Explore the most competitive interest rates available today, helping you make the most of your savings and investments.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Easy Check-in",
+    detail:
+    "Simplify your check-in process with our user-friendly platform, making arrival hassle-free and efficient.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+    "Discover unbeatable prices on top-quality products and services, ensuring you get the best value for your money.",
+  },
+];
+export default data;
